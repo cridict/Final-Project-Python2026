065df1ae2a9994ea54d32fd2aabc2f3631e5d9c0
Locks <JAnzalonejr12@gmail.com>
2026-05-04 14:04:00 -0400

    added asave function,search not done yet

--- SafeVault_Project/data_manager.py
+++ SafeVault_Project/data_manager.py
@@ -1,1 +1,26 @@
+#JSON / File Read and Write (20 pts)
 
+import json
+import os
+
+def add_to_json(site, code):
+    new_entry = {"website": site, "password": code}
+    path = "database/vault_data.json"
+
+    if os.path.exists(path) == False:
+        f = open(path, "w")
+        f.write("[]")
+        f.close()
+
+    f = open(path, "r")
+    data_list = json.load(f)
+    f.close()
+
+    data_list.append(new_entry)
+
+    f = open(path, "w")
+    json.dump(data_list, f, indent=4)
+    f.close()
+
+def find_password(site_name):
+    pass
