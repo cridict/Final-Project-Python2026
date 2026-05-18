f863959134accca1263351d44fc6b9db2baf962d
Catnip4383 <vinmecca08032011@gmail.com>
2026-05-18 14:14:06 -0400

    Update locker.py

--- SafeVault_Project/locker.py
+++ SafeVault_Project/locker.py
@@ -1,7 +1,12 @@
-#JSON / File Read and Write (20 pts)
- 
+#Password scrambling
+import base64
+
 def make_secret(text):
-    result = ""
-    for letter in text:
-        result = result + letter + "-"
-    return result
+    message_bytes = text.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    return base64_bytes.decode('ascii')
+
+def unscramble(text):
+    base64_bytes = text.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    return message_bytes.decode('ascii')
